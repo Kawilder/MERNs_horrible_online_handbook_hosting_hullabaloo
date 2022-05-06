dd36d151c37b62c5b4e1bb9cccc28a877c2b3666
Kawilder <kevin_wilde564@yahoo.com>
2022-05-06 18:59:46 -0500

    filled out mutations, queries, and added the apllo client to app.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+  { me {
+    _id
+    email
+    username
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }}
+`;
